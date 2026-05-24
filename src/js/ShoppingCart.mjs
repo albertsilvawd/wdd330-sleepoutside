dfc2b1ce290ee8b8a0f5face395980598896c880
albertsilvawd <avssilva@hotmail.com>
2026-05-23 21:12:43 -0300

    Refactor shopping cart to use ShoppingCart module

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,30 @@
+import { getLocalStorage, renderListWithTemplate } from './utils.mjs';
+
+function cartItemTemplate(item) {
+    return `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img src="${item.Images ? item.Images.PrimaryMedium : item.Image}" alt="${item.Name}" />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+}
+
+export default class ShoppingCart {
+    constructor(listElement) {
+        this.listElement = listElement;
+    }
+
+    init() {
+        const cartItems = getLocalStorage('so-cart') || [];
+        if (cartItems.length === 0) {
+            this.listElement.innerHTML = '<li>Your cart is empty.</li>';
+        } else {
+            renderListWithTemplate(cartItemTemplate, this.listElement, cartItems);
+        }
+    }
+}
